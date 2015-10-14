53eec51e2af20b1e8fb6a2e5cc964fa2e82fe5fe
Muhammad Kashif Abbasi <mkabbasi@goodgamestudios.com>
2015-10-14 17:23:04 +0200

    removed duplicates from array

--- intersectArray.php
+++ intersectArray.php
@@ -9,27 +9,23 @@
 function intersectArray($array1, $array2)
 {
     $intersectedArray = array();
-    $array1 = removeDuplicate($array1);
-    $array2 = removeDuplicate($array2);
-
+    // print_r($array2);
+    // exit;
     $i = 0;
     $j = 0;
 
-    // print_r($array1);exit;
-    while ($i < count($array1) && $j < count($array2)) {
+    while ($i <= count($array1) && $j <= count($array2)) {
         if ($array1[$i] < $array2[$j]) {
             ++$i;
         } elseif ($array2[$j] < $array1[$i]) {
             ++$j;
-        } elseif ($array2[$j] == $array1[$i]) {
-            // print_r($array2[$j++]);
-            // die("Died Here!");
+        } elseif ($array1[$i] == $array2[$j]) {
             $intersectedArray[] = $array2[$j];
             ++$i;
             ++$j;
         }
     }
-    print_r($intersectedArray);
+    return $intersectedArray;
 }
 function removeDuplicate($array)
 {
@@ -37,9 +33,9 @@ function removeDuplicate($array)
     foreach ($array as $key => $value) {
         $newArray[$value] = $value;
     }
-    return($newArray);
+    return $newArray;
 }
-
-$array1 = array(1,2,3,4,5,3,2);
-$array2 = array(2,3,5,7,3,2);
-intersectArray($array1, $array2);
+$array1 = array(5,6,4,1,2,2);
+$array2 = array(1,2,3,2,3,4,5);
+$intersectedArray = intersectArray(removeDuplicate($array1), removeDuplicate($array2));
+print_r($intersectedArray);
